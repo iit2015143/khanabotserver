9481cecb25951d7fda6f1614b811043911e74c0d
iit2015143 <iit2015143@iiita.ac.in>
2018-10-22 20:40:40 +0530

    Khanabot image edited

--- khanabotnights.js
+++ khanabotnights.js
@@ -20,7 +20,7 @@ var makkhans = {
 		"uptime" :20 ,
 		"downtime" : 5
 	},
-	"image":"khanabotnight.jpg",
+	"image":"khanabotnightte.jpg",
 	"number" :9956837774 ,
 	"feature" : "Value For Money",
 	"name" : "Khanabot Nights",
